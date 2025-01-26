133b0829456d8ce5ba3f7079625b8ba35048de57
Scott Murphy Heiberg <scott@alwaysvip.com>
2025-01-26 00:57:12 -0800

    Demonstrate working behavior

--- grails-app/init/resource/bug/BootStrap.groovy
+++ grails-app/init/resource/bug/BootStrap.groovy
@@ -1,8 +1,15 @@
 package resource.bug
 
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
+import grails.core.GrailsApplication
+
 class BootStrap {
 
+    GrailsApplication grailsApplication
+
     def init = { servletContext ->
+        println "This only fails on latest snapshot..."
+        new PathMatchingResourcePatternResolver(grailsApplication.classLoader).getResources("classpath*:messages_*.properties")
     }
     def destroy = {
     }
